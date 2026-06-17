fd3afaa0cf9d6178c839f4dd80a021a0712312ac
siddiqislam <129179176+siddiqislam@users.noreply.github.com>
2026-06-17 13:59:17 +0100

    Completed Python MVP with Nominatim.

--- meetup spot main code.py
+++ meetup spot main code.py
@@ -1,15 +1,19 @@
-# Import geopy library and Nominatim class
+import statistics # For finding mean of coordinates.
+
+# Import geopy library and Nominatim class.
 from geopy.geocoders import Nominatim
-# Call the Nominatim tool and create Nominatim class
+# Call the Nominatim tool and create Nominatim class.
 loc = Nominatim(user_agent="Geopy Library")
 geolocator = Nominatim(user_agent='myapplication')
 
 # User enters the addresses
-addlist = ["City of derry airport, northern ireland",
-           "80 Fen, city of london, UK",
-           "eiffel tower, france"]
+addlist = ["80 Fen, city of london, UK",
+           "Southfields station, London"]
 ### I would like to create a UI where the User inputs the addresses one by one.
 
+# Find midpoint by averaging latitudes and longitudes (consider using Northings/Easting instead?).
+# Create list of lats and longs for each address.
+norths, easts = list(), list()
 for addy in addlist:
     # Get address from geopy
     location = geolocator.geocode(addy, exactly_one=True, language="en", namedetails=True, addressdetails=True)
@@ -20,9 +24,13 @@ for addy in addlist:
         print("Not all of your addresses are located in the United Kingdom. This may skew results.\nAre you sure you want to continue?")
     ### I want to create UI that allows user to accept and continue or not.
 
-    # Print address
-    print(location.address)
-    # Print latitude and longitude
-    print(location.latitude, location.longitude)
+    # Fetch coordinates
+    north, east = location.latitude, location.longitude
+    norths.append(north)
+    easts.append(east)
 
+midpoint_north, midpoint_east = statistics.fmean(norths), statistics.fmean(easts)
+midpoint_as_string = str(midpoint_north) + ", " + str(midpoint_east)
+midpoint_address = geolocator.reverse(midpoint_as_string)
+print("Midpoint: ", midpoint_address)
 ### I would like to output a map showing all the addresses and the central point. The map should be interactive: scrollable and zoomable.
